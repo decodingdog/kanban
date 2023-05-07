63f5ad1fdff11002e0374a965db2d534bcbcfab5
u20230040249 <gowl4622@gmail.com>
2023-05-08 01:11:18 +0900

    feat: 메인 페이지 퍼블 작업

--- pages/index.vue
+++ pages/index.vue
@@ -1,85 +1,105 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
+  <v-row fluid :style="{ flexWrap: 'nowrap', overflowX: 'scroll' }">
+    <v-col
+      v-for="status in statusList"
+      :key="status.statusNm"
+      cols="12"
+      sm="4"
+      md="3"
+      class="mt-5 mx-2 pa-0"
+    >
+      <v-card color="#EEEEEE" class="pa-2">
+        <v-card-title :style="{ color: '#878787' }">
+          <span>{{ status.statusNm }}</span>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
+          <v-icon @click="delStatus">mdi-close</v-icon>
+        </v-card-title>
+        <v-btn
+          :style="{
+            width: '100%',
+            backgroundColor: 'initial',
+            boxShadow: 'none',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }"
+          @click="addTask"
+        >
+          <v-icon>mdi-add</v-icon>
+          <span>Add task</span>
+        </v-btn>
+        <v-card
+          v-for="task in status.tasks"
+          :key="`${status.statusNm}_${task.taskId}`"
+          class="my-2"
+        >
+          <v-card-title>{{ task.title }}</v-card-title>
+        </v-card>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "IndexPage",
-});
+interface Task {
+  taskId: number;
+  statusId: number;
+  title: string;
+  cn: string;
+  registDt: string;
+}
+
+interface Status {
+  statusId: number;
+  statusNm: string;
+  tasks: Task[];
+}
+
+export default {
+  setup() {
+    const statusList = ref<Status[]>([
+      {
+        statusId: 0,
+        statusNm: "Todo",
+        tasks: [
+          {
+            taskId: 0,
+            statusId: 0,
+            title: "dd",
+            cn: "dd",
+            registDt: "2022-03-10",
+          },
+        ],
+      },
+      {
+        statusId: 1,
+        statusNm: "In progress",
+        tasks: [],
+      },
+      {
+        statusId: 2,
+        statusNm: "Done",
+        tasks: [],
+      },
+      {
+        statusId: 3,
+        statusNm: "Test",
+        tasks: [],
+      },
+    ]);
+
+    const delStatus = () => {
+      console.log("delete status");
+    };
+
+    const addTask = () => {
+      console.log("add task");
+    };
+
+    return { statusList, delStatus, addTask };
+  },
+};
 </script>
+
+<style></style>
